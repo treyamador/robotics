867618bb6f0e18e5fde79e577ec93d1ae473b6d6
TreyAmador <tmamador@cpp.edu>
2017-03-04 13:25:39 -0800

    update input/output functionality

--- map.cc
+++ map.cc
@@ -9,8 +9,7 @@
 
 namespace {
 	
-	
-	
+	const std::string ENVIRONMENT = "hospital_section.pnm";
 	
 };
 
@@ -44,13 +43,15 @@ public:
 	void print_wavefront();
 	void print_map();
 	
-	bool destination_reached(PointXY& wf,Point2D& pos);
-	bool frontier_completed(PointXY& begin, PointXY& end);
+	void output_wavefront(const std::string& filepath);
+	
+	bool destination(PointXY& wf,Point2D& pos, bool cmplt);
+	bool frontier_completed(PointXY& wave, PointXY& end);
 	PointXY adjacent_greatest(PointXY& wf);
 	
-	bool right_open(PointXY& wf, PointXY& adj);
-	bool up_open(PointXY& wf, PointXY& adj);
-	bool left_open(PointXY& wf, PointXY& adj);
+	bool right_open(PointXY& wf, PointXY& adj, int grd);
+	bool up_open(PointXY& wf, PointXY& adj, int grd);
+	bool left_open(PointXY& wf, PointXY& adj, int grd);
 	
 	PointXY right_element(PointXY& wf, PointXY& adj);
 	PointXY up_element(PointXY& wf, PointXY& adj);
@@ -76,59 +77,91 @@ Map::~Map() {
 
 
 void Map::read(const std::string& filepath) {
-	std::ifstream in_file(filepath); 
-	std::string line;
+	std::string line, header;
+	std::ifstream in_file(filepath);
+	std::getline(in_file,header);
+	int width,height,max_val;
+	in_file >> width >> height >> max_val;
 	while (std::getline(in_file,line)) {
-		map_.push_back(std::vector<double>(line.size()));
-		wavefront_.push_back(std::vector<int>(line.size(),0));
-		for (size_t i = 0; i < map_.back().size(); ++i) {
-			map_.back()[i] = line[i]-'0';
-		}
+		map_= std::vector<std::vector<double> >(
+			height,std::vector<double>(width));
+		wavefront_ = std::vector<std::vector<int> >(
+			height,std::vector<int>(width,0));
+		for (size_t i = 0; i < line.size(); ++i)
+			map_[i/width][i%width] = static_cast<double>(line[i]+1);
 	}
-	
+	std::ofstream out_file("output.pnm");
+	out_file << width << " " << height << " " << max_val << std::endl;
+	for (size_t r = 0; r < map_.size(); ++r)
+		for (size_t c = 0; c < map_[r].size(); ++c)
+			out_file << (char)(map_[r][c]-1);
+}
+
+
+void Map::output_wavefront(const std::string& filepath) {
+
+	std::string line, header;
+	std::ifstream in_file(filepath);
+	std::getline(in_file,header);
+	int width,height,max_val;
+	in_file >> width >> height >> max_val;
+
+	std::ofstream out_file("wavefront.pnm");
+	out_file << header << std::endl;
+	out_file << width << " " << height << " " << max_val << std::endl;
+	for (size_t r = 0; r < wavefront_.size(); ++r)
+		for (size_t c = 0; c < wavefront_[r].size(); ++c)
+			out_file << (char)(wavefront_[r][c]-1);
 }
 
 
 PointXY Map::adjacent_greatest(PointXY& wf) {
-	int max_grad = 0, V = 3;
+	std::vector<PointXY> compass = {
+		PointXY(wf.x_,wf.y_-1),
+		PointXY(wf.x_,wf.y_+1),
+		PointXY(wf.x_-1,wf.y_),
+		PointXY(wf.x_+1,wf.y_)
+	};
+	int max_grd = 0;
 	PointXY adjacent = PointXY(0,0);
-	for (int i = 1; i < V*V; i+=2) {
-		int r = (wf.y_-1)+i/V,
-			c = (wf.x_-1)+i%V;
-		if (std::fabs(wavefront_[r][c]) > std::fabs(max_grad)) {
-			max_grad = wavefront_[r][c];
-			adjacent = PointXY(c,r);
+	for (std::vector<PointXY>::iterator iter = compass.begin(); 
+		iter != compass.end(); ++iter)
+	{
+		int curr_grd = wavefront_[iter->y_][iter->x_];
+		if (std::abs(curr_grd) > std::abs(max_grd)) {
+			adjacent = PointXY(iter->x_,iter->y_);
+			max_grd = curr_grd;
 		}
 	}
 	return adjacent;
 }
 
 
-bool Map::right_open(PointXY& wf, PointXY& adj) {
-	//PointXY right = this->right_element(wf,adj);
-	//return wavefront_[right.y_][right.x_] <= 0;
-	PointXY r = this->right_element(wf,adj);
-	return wavefront_[r.y_][r.x_] <= 0;
+bool Map::right_open(PointXY& wf, PointXY& adj, int grd) {
+	PointXY right = this->right_element(wf,adj);
+	return
+		wavefront_[right.y_][right.x_] <= 0 ||
+		std::abs(wavefront_[right.y_][right.x_]) == grd;
 }
 
 
-bool Map::up_open(PointXY& wf, PointXY& adj) {
+bool Map::up_open(PointXY& wf, PointXY& adj, int grd) {
 	PointXY up = this->up_element(wf,adj);
-	return wavefront_[up.y_][up.x_] <= 0;
+	return
+		wavefront_[up.y_][up.x_] <= 0 ||
+		std::abs(wavefront_[up.y_][up.x_]) == grd;
 }
 
 
-bool Map::left_open(PointXY& wf, PointXY& adj) {
+bool Map::left_open(PointXY& wf, PointXY& adj, int grd) {
 	PointXY left = this->left_element(wf,adj);
-	return wavefront_[left.y_][left.x_] <= 0;
+	return
+		wavefront_[left.y_][left.x_] <= 0 ||
+		std::abs(wavefront_[left.y_][left.x_]) == grd;
 }
 
 
 PointXY Map::right_element(PointXY& wf, PointXY& adj) {
-	//int col = adj.x_-wf.x_ == 1 ? 0 : adj.x_-wf.x_+1,
-	//	row = adj.y_-wf.y_ == 1 ? 0 : adj.y_-wf.y_+1;
-	//return PointXY(wf.x_+col,wf.y_+row);
-	
 	PointXY dspl = PointXY(adj.x_-wf.x_,adj.y_-wf.y_);
 	if (dspl.x_ == 0 && dspl.y_ == 1)
 		dspl = PointXY(1,0);
@@ -143,64 +176,63 @@ PointXY Map::right_element(PointXY& wf, PointXY& adj) {
 
 
 PointXY Map::up_element(PointXY& wf, PointXY& adj) {
-	PointXY diff = PointXY(-(adj.x_-wf.x_),-(adj.y_-wf.y_));
-	return PointXY(wf.x_+diff.x_,wf.y_+diff.y_);
+	return PointXY(
+		-(adj.x_-wf.x_)+wf.x_,
+		-(adj.y_-wf.y_)+wf.y_);
 }
 
 
 PointXY Map::left_element(PointXY& wf, PointXY& adj) {
-	int col = adj.x_-wf.x_ == -1 ? 0 : adj.x_-wf.x_-1,
-		row = adj.y_-wf.y_ == -1 ? 0 : adj.y_-wf.y_-1;
-	return PointXY(wf.x_+col,wf.y_+row);
+	PointXY dspl = PointXY(adj.x_-wf.x_,adj.y_-wf.y_);
+	if (dspl.x_ == 0 && dspl.y_ == 1)
+		dspl = PointXY(-1,0);
+	else if (dspl.x_ == 1 && dspl.y_ == 0)
+		dspl = PointXY(0,1);
+	else if (dspl.x_ == 0 && dspl.y_ == -1)
+		dspl = PointXY(1,0);
+	else if (dspl.x_ == -1 && dspl.y_ == 0)
+		dspl = PointXY(0,-1);
+	return PointXY(wf.x_+dspl.x_,wf.y_+dspl.y_);
 }
 
 
-void Map::wavefront(Point2D& player, Point2D& dest) {	
+void Map::wavefront(Point2D& player, Point2D& dest) {
 	int gradient = 1;
+	bool completed = false;
 	int dest_x = static_cast<int>(dest.x_),
 		dest_y = static_cast<int>(dest.y_);
 	wavefront_[dest_y][dest_x] = gradient;
-	while (gradient < 6) {
+	while (!completed) {
 		++gradient;
 		int init_y = dest_y;
 		while (wavefront_[init_y][dest_x] > 0)
 			--init_y;
 		PointXY wf = PointXY(dest_x,init_y),
-				end_wave = PointXY(wf.x_,wf.y_);
+				end_wave = PointXY(dest_x,init_y);
 		do {
-			if (map_[wf.y_][wf.x_] == 0)
-				wavefront_[wf.y_][wf.x_] = gradient;
-			else
-				wavefront_[wf.y_][wf.x_] = -gradient;
+			wavefront_[wf.y_][wf.x_] = 
+				map_[wf.y_][wf.x_] == 0 ? gradient : -gradient;
 			PointXY adj_wave = this->adjacent_greatest(wf);
-			
-			//std::cout << "\n( " << 
-			//	wf.x_ << " x " << wf.y_ << " ), " << " ( " << 
-			//	adj_wave.x_ << " x " << adj_wave.y_ << " )\n\n" << std::endl;
-			
-			if (this->right_open(wf,adj_wave))
+			if (this->right_open(wf,adj_wave,gradient))
 				wf = this->right_element(wf,adj_wave);
-			else if (this->up_open(wf,adj_wave))
+			else if (this->up_open(wf,adj_wave,gradient))
 				wf = this->up_element(wf,adj_wave);
-			else if (this->left_open(wf,adj_wave))
+			else if (this->left_open(wf,adj_wave,gradient))
 				wf = this->left_element(wf,adj_wave);
-			//else move back
+			completed = this->destination(wf,player,completed);	
 			
-			
-			//this->print_wavefront();
-			
-			
+			std::cout << wf.x_ << " " << wf.y_ << std::endl;
+					
 		} while (!this->frontier_completed(wf,end_wave));
-		
+		this->output_wavefront(ENVIRONMENT);
 	}
-	
 }
 
 
 void Map::print_map() {
 	for (size_t r = 0; r < map_.size(); ++r) {
 		for (size_t c = 0; c < map_[r].size(); ++c) {
-			std::cout << map_[r][c];
+			std::cout << map_[r][c] << " ";
 		}
 		std::cout << std::endl;
 	}
@@ -210,17 +242,10 @@ void Map::print_map() {
 
 
 void Map::print_wavefront() {
-	//for (size_t r = 0; r < wavefront_.size(); ++r) {
-	//	for (size_t c = 0; c < wavefront_[r].size(); ++c) {
-	//		std::cout << wavefront_[r][c] << " ";
-	//	}
-	//	std::cout << std::endl;
-	//}
-	//std::cout << std::endl << std::endl;
-	
 	for (size_t r = 0; r < wavefront_.size(); ++r) {
 		for (size_t c = 0; c < wavefront_[r].size(); ++c) {
-			std::cout << std::setw(3) << std::right << wavefront_[r][c];
+			std::cout << std::setw(3) << std::right << 
+				(map_[r][c] == 1 ? map_[r][c] : wavefront_[r][c]);
 		}
 		std::cout << "\n";
 	}
@@ -228,27 +253,29 @@ void Map::print_wavefront() {
 }
 
 
-bool Map::destination_reached(PointXY& wf, Point2D& pos) {
+bool Map::destination(PointXY& wf, Point2D& pos, bool cmplt) {
 	return
-		wf.x_ == static_cast<int>(pos.x_) && 
-		wf.y_ == static_cast<int>(pos.y_);
+		(wf.x_ == static_cast<int>(pos.x_) && 
+		wf.y_ == static_cast<int>(pos.y_)) || 
+		cmplt;
 }
 
 
-bool Map::frontier_completed(PointXY& begin, PointXY& end) {
-	return
-		begin.x_ == end.x_ &&
-		begin.y_ == end.y_;
+bool Map::frontier_completed(PointXY& wave, PointXY& end) {
+	return 
+		wave.x_ == end.x_ &&
+		wave.y_ == end.y_;
 }
 
 
 int main(int argc, char* argv[]) {
-	Point2D dest = Point2D(24.0,15.0);
-	Point2D player = Point2D(0.0,0.0);
+	Point2D dest = Point2D(500.0,200.0);
+	Point2D player = Point2D(100.0,100.0);
 	Map map;
-	//map.read("map.txt");
-	map.read("empty_map.txt");
+	map.read(ENVIRONMENT);
 	map.wavefront(player,dest);
+	std::cout << "\n\nCompleted\n\n" << std::endl;
+	map.output_wavefront(ENVIRONMENT);
 	return 0;
 }
 
